4da40f38d5182486ce9375ec8d6ae31245b3d266
ahh1539 <ahh1539@g.rit.edu>
2019-03-10 22:03:50 -0700

    continued progress on javafx gui

--- src/Game/GUI.java
+++ src/Game/GUI.java
@@ -6,10 +6,10 @@ import java.awt.*;
 public class GUI {
     private JFrame window;
     private Container cont;
-    private JPanel title;
+    private JPanel title, startButton;
     private JButton button;
     private JLabel label;
-    Font titlefont = new Font("Times New Roman", Font.BOLD,90);
+    Font titlefont = new Font("Times New Roman", Font.PLAIN,32);
 
     public GUI(){
 
@@ -18,7 +18,7 @@ public class GUI {
     public void MakeGUI() {
 
         window = new JFrame();
-        window.setSize(900, 800);
+        window.setSize(800, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.BLACK);
         window.setLayout(null);
@@ -26,17 +26,27 @@ public class GUI {
         cont = window.getContentPane();
 
         title = new JPanel();
-        title.setBounds(100, 100, 600, 100);
-        title.setForeground(Color.white);
+        title.setBounds(100, 100, 600, 150);
+        title.setBackground(Color.black);
         cont.add(title);
 
-        label = new JLabel();
-        label.setBackground(Color.BLACK);
-        label.setText("Welcome to Elder Scrolls Crawler!");
-        title.add(label);
+        label = new JLabel("Welcome to Elder Scrolls Crawler!");
+        label.setForeground(Color.white);
+        label.setFont(titlefont);
 
-        button = new JButton();
-        button.setText("Play");
+        title.add(label);
+        cont.add(title);
+
+        startButton = new JPanel();
+        startButton.setBounds(300,400,200,100);
+        startButton.setBackground(Color.blue);
+
+        button = new JButton("START GAME");
+        button.setBackground(Color.black);
+        button.setForeground(Color.white);
+
+        startButton.add(button);
+        cont.add(startButton);
 
     }
 }
